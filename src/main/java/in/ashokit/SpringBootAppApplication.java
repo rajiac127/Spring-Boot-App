31fd182605e886b5b579d628736c51868e1f222f
rajesh <rajesh.jobs27@gmail.com>
2021-11-15 13:46:43 +0530

    feature-301:Related Changes

--- src/main/java/in/ashokit/SpringBootAppApplication.java
+++ src/main/java/in/ashokit/SpringBootAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
+	Map<String, Object> map = new HashMap<String, Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
